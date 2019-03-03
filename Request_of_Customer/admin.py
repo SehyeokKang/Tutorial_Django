41aa2de4e1b3baa715d117a19771dfd47b6f3a47
Sehyeok_Kang <lasiel3@naver.com>
2019-03-04 01:56:45 +0900

    Tutorial app, first commit

---
+++ Request_of_Customer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post2
+
+admin.site.register(Post2)
+
+# Register your models here.
---
+++ Request_of_Customer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RequestOfCustomerConfig(AppConfig):
+    name = 'Request_of_Customer'
